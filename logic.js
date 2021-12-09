e15d10274fda0aac09fd85d94b0bb6991b565188
HarshRaj1707 <harshraj1707@gmail.com>
2021-12-09 11:34:22 +0530

    first commit

--- logic.js
+++ logic.js
@@ -9,3 +9,9 @@ function dec()
     let res=document.getElementById("a");
     res.innerHTML= parseInt(document.getElementById("a").innerHTML)-1;
 }
+
+function reset()
+{
+    let r=document.getElementById("a");
+    r.innerHTML=0;
+}
